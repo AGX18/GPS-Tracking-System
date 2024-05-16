e8cf7b8c7933ea1b162c02d2347bf78059589b36
Doaa-Said <168484790+Doaa-Said@users.noreply.github.com>
2024-05-16 23:16:06 +0300

    Update Leds&switches.c

--- Leds&switches.c
+++ Leds&switches.c
@@ -5,7 +5,7 @@ unsigned char readSW2(){
 	return (GPIO_PORTF_DATA_R &0x01);
 }	
 void Leds_output(unsigned char data){
-CLR(GPIO_PORTF_DATA_R,0x06);
+CLR(GPIO_PORTF_DATA_R,0x0E);
 	
 SET(GPIO_PORTF_DATA_R ,data);
 }
